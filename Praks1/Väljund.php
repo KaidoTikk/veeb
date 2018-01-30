3b5ed3ad2edbb335f453f7021341a314dd7fd9db
kaido.tikk <kaido.tikk@khk.ee>
2018-01-30 11:31:29 +0200

    Defineeriti algandmed

--- Praks1/Väljund.php
+++ Praks1/Väljund.php
@@ -22,7 +22,7 @@ $pereNimi = "Tikk"; // Perenimi, string
 $bussiNumber = 4; // bussi number, integer
 $soiduKestvus = 0.8; // sõidu kestvus, 0.8 tundi, float
 //konstantite defineerimine
-define('NUMBER_P1', 3,14);
+define('NUMBER_P1', 3.14);
 //Kasutades eel defineeritud muutujad ja konstandid
 //väljasta ilusa teksti
 echo 'Olen '.$eesNimi.' '.$pereNimi.'<br />';
@@ -31,9 +31,11 @@ echo 'Jõuan kesklinna '.$soiduKestvus.' tunniga<br />';
 echo 'Midagi konstantset? P1 = '.NUMBER_P1.'<br />';
 //tingimuslaused
 $ilmväljas = 'Sajab lund';
-if($ilmväljas == 'sajab lund'){
+if($ilmväljas == 'Sajab lund'){
     echo 'Tuleb minna sauna<br />';
 }
 else {
     echo 'Pole mõttet sauna minna<br />';
 }
+// ? operaatori - ternary operaator
+echo ($ilmväljas == 'Sajab lund') ? 'Tuleb minna sauna<br />' : 'Pole mõttet sauna minna<br />';
---
+++ Praks1/söögikalkulaator.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaido.tikk
+ * Date: 30.01.2018
+ * Time: 11:28
+ */
+//söögihind
+$soogihine = 2.55; // Hind, EUR
+$soodustuseProtsent = 15; //
+$opilasetoetus = 1.80; // EUR
