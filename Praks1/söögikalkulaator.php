922de9a69b68a6724703065a8158e35dfca93a55
kaido.tikk <kaido.tikk@khk.ee>
2018-01-30 11:37:04 +0200

    Arvutati söögihinda

--- Praks1/söögikalkulaator.php
+++ Praks1/söögikalkulaator.php
@@ -6,6 +6,12 @@
  * Time: 11:28
  */
 //söögihind
-$soogihine = 2.55; // Hind, EUR
+$soogihind = 2.55; // Hind, EUR
 $soodustuseProtsent = 15; //
 $opilasetoetus = 1.80; // EUR
+//arvestus
+if($soogihind > 0 and $soodustuseProtsent > o and $opilasetoetus > 0){
+    $soodushind =  $soogihind * (100 - $soodustuseProtsent) / 100;
+    $hindopilasele = $soodushind - $opilasetoetus;
+    echo 'Söögi hind opilasele = '.$hindopilasele.'<br />';
+}
