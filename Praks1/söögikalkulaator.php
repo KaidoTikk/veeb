cb572701fd7e44d5ec2e159bf7113f80343990da
kaido.tikk <kaido.tikk@khk.ee>
2018-02-08 11:24:26 +0200

    Massiivi loomine

--- Praks1/söögikalkulaator.php
+++ Praks1/söögikalkulaator.php
@@ -25,13 +25,21 @@ function soogihind($taishind, $sooduskaart = false, $kasoledopilane = false){
     }
     return $soodushind;
 }//funktsioooni lõpp
+
+//Testimiseks paneme erinevad väärtused paika
+// kasutame selleks  massiivi kujul(sooduskarrt, kasoledõpilane)
+$opilane = array(true,true); // olemas soodus - $opilane[0] kui ka õpilane
+$opetaja = array(true, false); // olemas soodus aga õpilane ei ole
+$kulaline = array(false, false); // pole soodustust ega pole õpilane
+
+
 // kutsume funktsiooni tööle
 //kui oled õpilane
-$soogihind = soogihind( 2.65,  true,  true);
+$soogihind = soogihind( 2.65,$opilane[0],$opilane[1]);
 echo 'Prae hind õpilasele = '.round($soogihind, 2).'<br />';
 //Prae hind omanikule
-$soogihind = soogihind( 2.65,  true,  false);
+$soogihind = soogihind( 2.65, $opetaja[0], $opetaja[1]);
 echo 'Prae hind omanikule = '.round($soogihind, 2).'<br />';
 //Prae täishind
-$soogihind = soogihind( 2.65);
+$soogihind = soogihind( 2.65, $kulaline[0], $kulaline[1]);
 echo 'Prae hind = '.round($soogihind, 2).'<br />';
