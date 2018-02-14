e1858b490eab474db9319393f30e73db87be46a5
kaido.tikk <kaido.tikk@khk.ee>
2018-02-14 13:39:04 +0200

    Lisati arvu arvamise vorm

---
+++ Praks1/mang.uus.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaido.tikk
+ * Date: 14.02.2018
+ * Time: 13:37
+ */
+echo '
+   <form method="post" action="'.$_SERVER['PHP_SELF'].'">
+        Sisesta arv vahemikus 0-50: <input type="number"><br />
+       <input type="submit" value="Kontrolli">
+    </form>
+';
