cd15bbb1c022835901e25732b8ad6197bd3ea564
kaido.tikk <kaido.tikk@khk.ee>
2018-02-14 13:41:06 +0200

    Lisati arvu jälgimine serveris

--- Praks1/mang.uus.php
+++ Praks1/mang.uus.php
@@ -6,8 +6,16 @@
  * Time: 13:37
  */
 echo '
-   <form method="post" action="'.$_SERVER['PHP_SELF'].'">
-        Sisesta arv vahemikus 0-50: <input type="number"><br />
-       <input type="submit" value="Kontrolli">
-    </form>
+     <form method="post" action="'.$_SERVER['PHP_SELF'].'">
+        Sisesta arv vahemikus 0-50: 
+        <input type="number" name="kasutajaArv"><br />
+         <input type="submit" value="Kontrolli">
+     </form>
 ';
+
+
+// määrame arvu
+$serveriArv = 10;
+$kasutajaArv = $_POST['kasutajaArv'];
+// kontrollime kasutaja väärtus
+echo $kasutajaArv;
