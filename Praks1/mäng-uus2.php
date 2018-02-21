06ab13c5e507ba3693ede06ceb063257a9e32642
kaido.tikk <kaido.tikk@khk.ee>
2018-02-21 09:26:09 +0200

    Arvu lisamine.

---
+++ Praks1/mäng-uus2.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaido.tikk
+ * Date: 21.02.2018
+ * Time: 9:24
+ */
+// määrame katsete arvu muutujat
+$katseteArv = $_POST['katseteArv'];
+// määrame serveri arvu muutujat
+$serveriArv = $_POST['serveriArv'];
+// kontrollime antud väärtuse olemasolu
+$katseteArv = isset($katseteArv) ? ++$katseteArv : 0;
+$serveriArv = isset($serveriArv) ? $serveriArv : rand(1, 50);
+// lisame kasutusele sessiooni
+session_start();
+if(!isset($_SESSION['serveriArv'])){
+        $_SESSION['serveriArv'] = rand(1, 50);
+    } else {
+        $serveriArv = $_SESSION['serveriArv'];
+    }
+// kontrollimiseks vaatame $_SESSION massiivi
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+ // trükime andmetega täidetud vorm
+ echo '
+     <form method="post" action="'.$_SERVER['PHP_SELF'].'">
+         Sisesta arv vahemikus 1-50: 
+         <input type="number" name="kasutajaArv"><br />
+       <input type="hidden" name="katseteArv" value="'.$katseteArv.'">
+       <input type="hidden" name="serveriArv" value="'.$serveriArv.'">
+         <input type="submit" value="Kontrolli">
+     </form>
+ ';
+ // kontrollime, kas arv on edastatud
+ if(!empty($_POST['kasutajaArv'])){
+     // määrame arvu
+     $kasutajaArv = $_POST['kasutajaArv'];
+     if($kasutajaArv > $serveriArv){
+         echo 'Pakutud väärtus on suurem<br />';
+     }
+     if ($kasutajaArv < $serveriArv) {
+         echo 'Pakutud väärtus on väiksem<br />';
+     }
+     if (abs($kasutajaArv-$serveriArv)<=5){
+         if($kasutajaArv == $serveriArv){
+             echo 'Arvasid ära!<br />';
+             echo 'Arv on '.$serveriArv.'<br />';
+             echo 'Arvu ära aramiseks läks '.$katseteArv.' korda<br />';
+             exit;
+         }
+         echo 'Oled juba hästi lähedal<br />';
+     }
+ } else {
+     echo 'Arv peab olema sisestatud!';
+ }
