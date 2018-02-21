6209737152ee83fc4816f173301b0fd51ac44281
kaido.tikk <kaido.tikk@khk.ee>
2018-02-21 09:09:42 +0200

    lõpp lahendus

--- Praks1/mang.uus.php
+++ Praks1/mang.uus.php
@@ -6,36 +6,34 @@
  * Time: 13:37
  */
 $katsetearv = $_POST['katsetearv'];
+$serveriarv = $_POST['serveriarve'];
 $katsetearv = isset($katsetearv) ? ++$katsetearv : 0;
+$serveriarv = isset($serveriarv) ? $serveriarv : rand(1, 50);
 echo '
      <form method="post" action="'.$_SERVER['PHP_SELF'].'">
         Sisesta arv vahemikus 0-50: 
          <input type="number" name="kasutajaArv"><br />
          <input type="hidden" name="katsetearv" value="'.$katsetearv.'">
+         <input type="hidden" name="serveriarv" value="'.$serveriarv.'">
          <input type="submit" value="Kontrolli">
      </form>
  ';
 
-// määrame arvu
-$serveriArv = 10;
-$kasutajaArv = $_POST['kasutajaArv'];
-// kontrollime kasutaja väärtus
-echo $kasutajaArv;
 // kontrollime, kas arv on edastatud
 if(!empty($_POST['kasutajaArv'])){
        // määrame arvu
-       $serveriArv = 10;
+      // $serveriArv = 10;
        $kasutajaArv = $_POST['kasutajaArv'];
-       if($kasutajaArv > $serveriArv){
+       if($kasutajaArv > $serveriarv){
                echo 'Pakutud väärtus on suurem<br />';
    }
-   if ($kasutajaArv < $serveriArv) {
+   if ($kasutajaArv < $serveriarv) {
                echo 'Pakutud väärtus on väiksem<br />';
    }
-    if (abs($kasutajaArv-$serveriArv)<=5){
-        if($kasutajaArv == $serveriArv){
+    if (abs($kasutajaArv-$serveriarv)<=5){
+        if($kasutajaArv == $serveriarv){
             echo 'Arvasid ära!<br />';
-            echo 'Arv on '.$serveriArv.'<br />';
+            echo 'Arv on '.$serveriarv.'<br />';
             echo 'Arvu ära aramiseks läks '.$katsetearv.' korda<br />';
             exit;
         }
