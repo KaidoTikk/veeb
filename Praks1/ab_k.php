49bc0caa9a771a2f9c0cb4fb50963da5ad74cca8
kaido.tikk <kaido.tikk@khk.ee>
2018-02-22 13:36:05 +0200

    Lisati andmebaasi serveriga ühendamine

---
+++ Praks1/ab_k.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaido.tikk
+ * Date: 22.02.2018
+ * Time: 13:21
+ */
+define('AB_HOST', 'localhost');
+define('AB_USER', 'iktkhk01_kaido');
+define('AB_PASS', '^BSw6HXzSQ}j');
+define('AB_NIMI', 'iktkhk01_andmed');
